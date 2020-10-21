39b86ae4f0434c1d03bbc9e61c0fecc86b3db009
siskomen <az23634@gmail.com>
2020-10-21 22:55:30 +0300

    Made a tourOperator home page particulary

--- src/Controller/MuseumController.php
+++ src/Controller/MuseumController.php
@@ -10,10 +10,29 @@ class MuseumController extends AbstractController
     /**
      * @Route("/museum", name="museum")
      */
-    public function index()
+    public function home()
     {
-        return $this->render('museum/index.html.twig', [
+        return $this->render('museum/museumBase.html.twig', [
             'controller_name' => 'MuseumController',
         ]);
     }
+    /**
+     * @Route("/museum/settings", name="museum_settings")
+     */
+    public function museumSettings()
+    {
+        return $this->render('museum/settings/settings.html.twig', [
+            'controller_name' => 'MuseumController',
+        ]);
+    }
+    /**
+     * @Route("/museum/schedule", name="museum_schedule")
+     */
+    public function schedule()
+    {
+        return $this->render('museum/schedule/schedule.html.twig', [
+            'controller_name' => 'MuseumController',
+        ]);
+    }
+
 }
--- src/Controller/TourOperatorController.php
+++ src/Controller/TourOperatorController.php
@@ -8,12 +8,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class TourOperatorController extends AbstractController
 {
     /**
-     * @Route("/tour/operator", name="tour_operator")
+     * @Route("/tourOperator", name="tour_operator")
      */
-    public function index()
+    public function home()
     {
-        return $this->render('tour_operator/index.html.twig', [
+        return $this->render('tour_operator/home/index.html.twig', [
             'controller_name' => 'TourOperatorController',
         ]);
     }
+    /**
+     * @Route("/tourOperator/settings", name="tour_operator_schedule")
+     */
+    public function settings()
+    {
+        return $this->render('tour_operator/schedule/schedule.html.twig', [
+            'controller_name' => 'TourOperatorController',
+        ]);
+    }
+    /**
+     * @Route("/tourOperator/museum", name="tour_operator_settings")
+     */
+    public function museum()
+    {
+        return $this->render('tour_operator/settings/settings.html.twig', [
+            'controller_name' => 'TourOperatorController',
+        ]);
+    }
+
 }
